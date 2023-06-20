321d39f79d6a37374c2c0e758aae3437eaa8df0f
Maxime Silverio <maxime.silverio-dias@epitech.eu>
2023-06-20 12:47:04 +0200

    [ADD] Init map generator files

---
+++ include/Game/MapGenerator.hpp
@@ -0,0 +1,13 @@
+#ifndef __SANDBOX_MAP_GENERATOR__
+#define __SANDBOX_MAP_GENERATOR__
+
+namespace SandBox {
+    class MapGenerator {
+        public:
+            MapGenerator();
+            ~MapGenerator();
+        private:
+    };
+}; // namespace SandBox
+
+#endif /*__SANDBOX_MAP_GENERATOR__*/
---
+++ src/Game/MapGenerator.cpp
@@ -0,0 +1,13 @@
+/*
+**  Epitech Pre-MSC 2023
+**  End Year Project - Sandbox
+*/
+
+#include "MapGenerator.hpp"
+
+SandBox::MapGenerator::MapGenerator() {
+
+}
+
+SandBox::MapGenerator::~MapGenerator() {
+}
