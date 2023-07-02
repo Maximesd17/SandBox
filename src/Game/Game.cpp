6690b5f034476c2151fc5e4d4280dac2d1390099
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2023-07-03 00:30:28 +0200

    [ADD] generation de texture avec un textfile

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -39,6 +39,7 @@ void MySandBox::Game::reset()
 
 void MySandBox::Game::displayMap()
 {
+ /*
     sf::Vector2u size = _window.getSize();
     sf::Vector2u pos = sf::Vector2u(0, size.y - 64 * 2);
 
@@ -46,6 +47,86 @@ void MySandBox::Game::displayMap()
         _s_wall.setPosition(pos.x, pos.y);
         _window.draw(_s_wall);
         pos.x += 64;
+    }*/
+
+    std::ifstream mapFile("maps/small.txt");
+    std::vector<std::string> mapData;
+    std::string line;
+    while (std::getline(mapFile, line)) {
+        mapData.push_back(line);
+    }
+
+
+    mapFile.close();
+
+    std::vector<sf::Sprite> tiles;
+
+    sf::Texture airTexture;
+    airTexture.loadFromFile("resources/air.png");
+
+    sf::Texture groundTexture;
+    groundTexture.loadFromFile("resources/ground.png");
+
+    sf::Texture plateformTexture;
+    plateformTexture.loadFromFile("resources/plateform.png");
+
+    sf::Texture wallTexture;
+    wallTexture.loadFromFile("resources/textures.png");
+
+    sf::Texture boxTexture;
+    boxTexture.loadFromFile("resources/plateform.png");
+
+    sf::Texture spawnTexture;
+    spawnTexture.loadFromFile("resources/air.png");
+
+    sf::Texture endTexture;
+    endTexture.loadFromFile("resources/air.png");
+
+       for (size_t y = 0; y < mapData.size(); ++y) {
+        const std::string& line = mapData[y];
+        for (size_t x = 0; x < line.size(); ++x) {
+            char ch = line[x];
+
+            sf::Sprite tileSprite;
+
+            switch (ch) {
+                case '0':
+                    tileSprite.setTexture(airTexture);
+                    break;
+                case 'G':
+                    tileSprite.setTexture(groundTexture);
+                    break;
+                case 'P':
+                    tileSprite.setTexture(plateformTexture);
+                    break;
+                case 'W':
+                    tileSprite.setTexture(wallTexture);
+                    break;
+                case 'B':
+                    tileSprite.setTexture(boxTexture);
+                    break;
+                case 'S':
+                    tileSprite.setTexture(spawnTexture);
+                    tileSprite.setPosition(x * 32 * 4, y * 32 * 4);
+                    break;
+                case 'E':
+                    tileSprite.setTexture(endTexture);
+                    break;
+
+                default:
+                    continue;
+            }
+
+            tileSprite.setScale(4, 4);
+
+            tileSprite.setPosition(x * 32 * 4, y * 32 * 4);
+
+            tiles.push_back(tileSprite);
+
+        }
+    }
+    for (const sf::Sprite& tile : tiles) {
+    _window.draw(tile);
     }
 }
 
