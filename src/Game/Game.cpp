f3b7e8a500ffa160cd2fa5a31fbe4258dcf153a2
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2023-07-03 12:07:27 +0200

    [ADD] map_texture folder

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -39,7 +39,7 @@ void MySandBox::Game::Game::reset()
 {
     std::string line;
 
-    _t_wall.loadFromFile("resources/textures.png", sf::IntRect(0, 0, 64, 64));
+    _t_wall.loadFromFile("resources/map_textures/wall.png", sf::IntRect(0, 0, 64, 64));
     _sprite_shit.loadFromFile("resources/player.png");
 
     _player.setPlayerSprites(_sprite_shit);
@@ -52,6 +52,7 @@ void MySandBox::Game::Game::reset()
 /*********displayMap*********/
 void MySandBox::Game::Game::displayMap()
 {
+/*
     sf::Vector2u size = _window.getSize();
     sf::Vector2u pos = sf::Vector2u(0, size.y - 64 * 2);
 
@@ -59,6 +60,86 @@ void MySandBox::Game::Game::displayMap()
         _s_wall.setPosition(pos.x, pos.y);
         _window.draw(_s_wall);
         pos.x += 64;
+    }*/
+
+    std::ifstream mapFile("maps/small.txt");
+    std::vector<std::string> mapData;
+    std::string line;
+    while (std::getline(mapFile, line)) {
+        mapData.push_back(line);
+    }
+
+
+    mapFile.close();
+
+    std::vector<sf::Sprite> tiles;
+
+    sf::Texture airTexture;
+    airTexture.loadFromFile("resources/map_textures/air.png");
+
+    sf::Texture groundTexture;
+    groundTexture.loadFromFile("resources/map_textures/ground.png");
+
+    sf::Texture plateformTexture;
+    plateformTexture.loadFromFile("resources/map_textures/plateform.png");
+
+    sf::Texture wallTexture;
+    wallTexture.loadFromFile("resources/map_textures/wall.png");
+
+    sf::Texture boxTexture;
+    boxTexture.loadFromFile("resources/map_textures/box.png");
+
+    sf::Texture spawnTexture;
+    spawnTexture.loadFromFile("resources/map_textures/start.png");
+
+    sf::Texture endTexture;
+    endTexture.loadFromFile("resources/map_textures/end.png");
+
+       for (size_t y = 0; y < mapData.size(); ++y) {
+        const std::string& line = mapData[y];
+        for (size_t x = 0; x < line.size(); ++x) {
+            char ch = line[x];
+
+            sf::Sprite tileSprite;
+
+            switch (ch) {
+                case '0':
+                    tileSprite.setTexture(airTexture);
+                    break;
+                case 'G':
+                    tileSprite.setTexture(groundTexture);
+                    break;
+                case 'P':
+                    tileSprite.setTexture(plateformTexture);
+                    break;
+                case 'W':
+                    tileSprite.setTexture(wallTexture);
+                    break;
+                case 'B':
+                    tileSprite.setTexture(boxTexture);
+                    break;
+                case 'S':
+                    tileSprite.setTexture(spawnTexture);
+                    tileSprite.setPosition(x * 32 * 4, y * 32 * 4);
+                    break;
+                case 'E':
+                    tileSprite.setTexture(endTexture);
+                    break;
+
+                default:
+                    continue;
+            }
+
+            tileSprite.setScale(4, 4);
+
+            tileSprite.setPosition(x * 32 * 4, y * 32 * 4);
+
+            tiles.push_back(tileSprite);
+
+        }
+    }
+    for (const sf::Sprite& tile : tiles) {
+    _window.draw(tile);
     }
 }
 
